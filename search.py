53e8f026c0a293d773e29325334b2cac380c5702
Jack <124092777+MrJack100@users.noreply.github.com>
2023-03-25 16:16:39 +0000

    Managed to get search function to work and return JSON data

--- search.py
+++ search.py
@@ -1,22 +1,69 @@
-import requests
+import requests, json
 from bs4 import BeautifulSoup
 
-searchterm = ("budget")
+def search(searchterm):
+	url_safe_prompt = searchterm.replace(" ", "%20")
+	
+	url = ("https://www.dailymail.co.uk/home/search.html?sel=site&searchPhrase=" + url_safe_prompt)
+	response = requests.get(url)
+	
+	response.encoding = response.apparent_encoding
+	
+	soup = BeautifulSoup(response.text, "html.parser")
+	
+	main_soup_list = soup.find_all("div", {"class": "sch-result"})
+	
+	main_soup_str = ''.join([str(item) for item in main_soup_list])
+	
+	main_soup = BeautifulSoup((str(main_soup_str)), "html.parser")
+	
+	h3_list = main_soup.find_all("h3")
+	
+	h3_str = ''.join([str(item) for item in h3_list])
+	
+	h3_soup = BeautifulSoup(h3_str, "html.parser")
+	
+	###
+	
+	links = []
+	for a in h3_soup.find_all('a', href=True):
+	    links.append(a['href'])
+	
+	###
+	
+	headings = []
+	for heading in h3_soup.find_all('h3'):
+		headings.append((heading.get_text()).strip())
+	
+	###
+	
+	images = []
+	for img in main_soup.find_all('img', src=True):
+		images.append(img['src'])
+	
+	return([links, headings, images])
 
-url_safe_prompt = searchterm.replace(" ", "%20")
 
-url = ("https://www.dailymail.co.uk/home/search.html?sel=site&searchPhrase=" + url_safe_prompt)
-response = requests.get(url)
+x = search("budget")
 
-response.encoding = response.apparent_encoding
+links = x[0]
+headings = x[1]
+images = x[2]
 
-soup = BeautifulSoup(response.text, "html.parser")
+increment = 0
 
-main_soup_list = soup.find_all("div", {"class": "sch-result"})
+final = []
 
-main_soup_str = ''.join([str(item) for item in main_soup_list])
+while True:
+	try:
+		y = []
+		y.append(links[increment])
+		y.append(headings[increment])
+		y.append(images[increment])
+		final.append(y)
+		increment += 1
+	except:
+		break
 
-main_soup = BeautifulSoup((str(main_soup_str)), "html.parser")
-
-with open("testresponse.html", "w") as f:
-	f.write((str(main_soup_str)))
+with open("test.json", "w") as f:
+	f.write((json.dumps(final)))
