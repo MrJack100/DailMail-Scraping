8d5e06d29fd6b3683614678060a5582183c97792
Jack <124092777+MrJack100@users.noreply.github.com>
2023-03-25 17:28:36 +0000

    Fixed issues with dailymailclass and updated main.py (Have not tested yet)

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ print(mail.homepage)
 # Prints search results for "fire"
 mail.search("fire")
 print(mail.searchresult)
+
+# Prints content of "https://www.dailymail.co.uk/news/article-11858277/HS2-created-ghost-towns-Britain-homeowners-forced-beloved-properties.html"
+mail.article("https://www.dailymail.co.uk/news/article-11858277/HS2-created-ghost-towns-Britain-homeowners-forced-beloved-properties.html")
+print(mail.articlecontent)
